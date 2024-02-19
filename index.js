bca82c28adebcb129ba391ce4bee2d6f4dd7e51c
ElGunner79 <richard.brazao@outlook.com>
2024-02-19 22:05:32 +0800

    Added layout for server side front end, using bootstrap & handlebars

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const handlebars = require('express-handlebars');
 const {handleInvalidJson, handleUnauthorized, handleNotFound, handleAllOtherErrors} = require("./errors/errorHandler");
 const morganMiddleware = require("./logging/morganMiddleware");
 const Logger = require("./logging/logger");
@@ -12,7 +13,19 @@ models.init();
 
 const app = express();
 
+app.set('view engine', 'hbs');
+
+app.engine('hbs', handlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+    defaultLayout: 'main',
+    extname: 'hbs'
+}));
+
 app.use(express.json());
+
+//Serves static files (needed to import a css file in a react app)
+app.use(express.static('public'));
 
 app.use(morganMiddleware);
 
@@ -25,12 +38,15 @@ if(process.env.NODE_ENV === "development") {
 
 //Routes
 app.use("/api/users", require("./routes/userRoutes"));
+// add video routes
 app.use("/api/videos", require("./routes/videoRoutes"));
+// add like routes
 app.use("/api/likes", require("./routes/likeRoutes"));
+// add comment routes
 app.use("/api/comments", require("./routes/commentRoutes"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render('main', { layout: 'index' });
 });
 
 //Added error handler middleware functions to the pipeline
