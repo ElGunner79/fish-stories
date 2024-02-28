efbea362199e0d673a93b90e4ad943396c3b7ab2
ElGunner79 <richard.brazao@outlook.com>
2024-02-28 21:38:14 +0800

    updated the tabbing in the user validator

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -4,10 +4,7 @@ const userValidator = [
     body("name", "name is required").not().isEmpty(),
     body("email", "email is required").not().isEmpty(),
     body("email", "Invalid email").isEmail(),
-    body(
-        "password",
-        "The minimum password length is 6 characters, max 50"
-    ).isLength({ min: 6, max: 50 }),
+    body("password", "The minimum password length is 6 characters, max 50").isLength({ min: 6, max: 50 }),
 ];
 
 const userUpdateValidator = [
